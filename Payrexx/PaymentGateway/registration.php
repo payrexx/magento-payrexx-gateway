08c5c97566975dcc139d395c88310cc3b620ccc9
vinoth_ss4u <vinoth@softsolutions4u.info>
2018-03-22 18:48:10 +0530

    Created the module "Payrexx_PaymentGateway".

---
+++ Payrexx/PaymentGateway/registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Payrexx Payment Gateway
+ *
+ * Copyright © 2018 PAYREXX AG (https://www.payrexx.com)
+ * See LICENSE.txt for license details.
+ *
+ * @author SoftSolutions4U <info@softsolutions4u.com>
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Payrexx_PaymentGateway',
+    __DIR__
+);
+
