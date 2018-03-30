cede22cae1d515d6285fbb17938e19045bba87ef
vinothss4u <vinoth@softsolutions4u.info>
2018-03-30 18:36:32 +0530

    Implemented the functionality to create the gateway and get the web hook response.

---
+++ Payrexx/PaymentGateway/Controller/AbstractAction.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * Payrexx Payment Gateway
+ *
+ * Copyright © 2018 PAYREXX AG (https://www.payrexx.com)
+ * See LICENSE.txt for license details.
+ *
+ * @author SoftSolutions4U <info@softsolutions4u.com>
+ */
+namespace Payrexx\PaymentGateway\Controller;
+
+/**
+ * AbstractAction class is base class for Payrexx Payment Getaway
+ */
+abstract class AbstractAction extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * Uses additional_information as storage
+     */
+    const PAYMENT_GATEWAY_ID    = 'payrexx_gateway_id';
+
+    /**
+     * Uses additional_information as storage
+     */
+    const PAYMENT_SECURITY_HASH  = 'payrexx_security_hash';
+
+    /**
+     * @var \Magento\Framework\App\Action\Context
+     */
+    protected $context;
+
+    /**
+     * @var \Magento\Checkout\Model\Session
+     */
+    protected $checkoutSession;
+
+    /**
+     * @var \Magento\Sales\Model\OrderFactory
+     */
+    protected $orderFactory;
+
+    /**
+     * @var \Magento\Directory\Model\CountryFactory
+     */
+    protected $countryFactory;
+
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $configSettings;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\App\Action\Context              $context
+     * @param \Magento\Checkout\Model\Session                    $checkoutSession
+     * @param \Magento\Sales\Model\OrderFactory                  $orderFactory
+     * @param \Payrexx\PaymentGateway\Helper\Checkout            $checkoutHelper
+     * @param \Magento\Directory\Model\CountryFactory            $countryFactory
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $configSettings
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context              $context,
+        \Magento\Checkout\Model\Session                    $checkoutSession,
+        \Magento\Sales\Model\OrderFactory                  $orderFactory,
+        \Payrexx\PaymentGateway\Helper\Checkout            $checkoutHelper,
+        \Magento\Directory\Model\CountryFactory            $countryFactory,
+        \Magento\Framework\App\Config\ScopeConfigInterface $configSettings
+    ) {
+        parent::__construct($context);
+        $this->context          = $context;
+        $this->checkoutSession  = $checkoutSession;
+        $this->orderFactory     = $orderFactory;
+        $this->checkoutHelper   = $checkoutHelper;
+        $this->countryFactory   = $countryFactory;
+        $this->configSettings   = $configSettings;
+        $this->registerPayrexxApi();
+    }
+
+    /**
+     * Load Payrexx Api files
+     */
+    public function registerPayrexxApi()
+    {
+        require_once dirname(__DIR__) . '/vendor/autoload.php';
+    }
+
+    /**
+     * Get the Payrexx configuration hold for Merchant configuration
+     *
+     * @return \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    public function getPayrexxConfig()
+    {
+        return $this->configSettings->getValue(
+            'payment/payrexx_payment'
+        );
+    }
+
+    /**
+     * This function is redirect to cart after customer is cancel the payment.
+     */
+    public function executeCancelAction()
+    {
+        if ($this->checkoutHelper->cancelCurrentOrder('')) {
+            $this->checkoutHelper->restoreQuote();
+        }
+        $this->redirectToCheckoutCart();
+    }
+
+    /**
+     * Redirect to cart when and restored the previous selected Item.
+     */
+    public function redirectToCheckoutCart()
+    {
+        $this->_redirect('checkout/cart');
+    }
+
+    /**
+     * Get Order object
+     *
+     * @param integer $orderId Order id
+     * @return \Magento\Sales\Model\Order
+     */
+    public function getOrderDetailByOrderId($orderId)
+    {
+        $order = $this->orderFactory
+            ->create()
+            ->loadByIncrementId($orderId);
+        if (!$order->getId()) {
+            return null;
+        }
+        return $order;
+    }
+
+    /**
+     * Creates Payrexx Instance from the given credentials
+     *
+     * @return \Payrexx\Payrexx
+     */
+    public function getPayrexxInstance()
+    {
+        $config = $this->getPayrexxConfig();
+        return new \Payrexx\Payrexx(
+            $config['instance_name'],
+            $config['api_secret']
+        );
+    }
+}
--- Payrexx/PaymentGateway/Controller/Payment/Redirect.php
+++ Payrexx/PaymentGateway/Controller/Payment/Redirect.php
@@ -13,25 +13,157 @@ namespace Payrexx\PaymentGateway\Controller\Payment;
  * class \Payrexx\PaymentGateway\Controller\Payment\Redirect
  * Redirect controller to access from frontend
  */
-class Redirect extends \Magento\Framework\App\Action\Action
+class Redirect extends \Payrexx\PaymentGateway\Controller\AbstractAction
 {
     /**
      * Return result based on the request
      */
     public function execute()
     {
-        $paymentUrl = $this->getPaymentUrl();
-        if ($paymentUrl) {
-            $this->_redirect($paymentUrl);
+        // Get current order detail from checkoutsession object.
+        $orderId = $this->checkoutSession->getLastRealOrderId();
+        if (empty($orderId)) {
+            $this->redirectToCheckoutCart();
+            return;
+        }
+
+        $order   = $this->getOrderDetailByOrderId($orderId);
+        if (!$order) {
+            $this->redirectToCheckoutCart();
+            return;
+        }
+
+        // Create payrexx gateway using Payrexx Api
+        $response = $this->createPayrexxGateway($order);
+        if ($response) {
+            $this->setPaymentAdditionalInfo($order->getPayment(), $response);
+            // Fix the redirect URL
+            $payrexxUrl = $this->resolvePayrexxUrl($response->getLink());
+            $this->_redirect($payrexxUrl);
+            return;
+        }
+
+        // If any exception occured cancel the current order
+        // Add the ordered product into cart
+        $this->context->getMessageManager()->addError(
+            __('An error occurred while processing your payment. Please try again later.')
+        );
+        $this->executeCancelAction();
+    }
+
+    /**
+     * Create Payrexx Gateway
+     *
+     * @param  \Magento\Sales\Model\Order $order The order related details
+     * @return \Payrexx\Models\Response\Gateway
+     */
+    public function createPayrexxGateway($order)
+    {
+        // Create payrexx gateway object
+        $gateway = new \Payrexx\Models\Request\Gateway();
+
+        $gateway->setPsp(array());
+        $gateway->setAmount($order->getGrandTotal() * 100);
+        $gateway->setCurrency($order->getOrderCurrencyCode());
+
+        // Set order id as the reference id
+        $gateway->setReferenceId($order->getRealOrderId());
+        // Set success page url to redirect after successfull payment
+        $gateway->setSuccessRedirectUrl(
+            $this->_url->getUrl('checkout/onepage/success')
+        );
+        // Set failure page url to redirect after unsuccessfull payment
+        $gateway->setFailedRedirectUrl(
+            $this->_url->getUrl('checkout/cart')
+        );
+
+        $billingAddress = $order->getBillingAddress();
+
+        // Get country from country factory
+        $countryFactory = $this->countryFactory
+            ->create()
+            ->loadByCode($billingAddress->getCountryId());
+
+        // Contact information which should be stored along with payment
+        $fields = [
+            'title'     => '',
+            'forename'  => $billingAddress->getFirstname(),
+            'surname'   => $billingAddress->getLastname(),
+            'company'   => $billingAddress->getCompany(),
+            'street'    => implode(',', $billingAddress->getStreet()),
+            'postcode'  => $billingAddress->getPostcode(),
+            'place'     => $billingAddress->getRegion(),
+            'country'   => $countryFactory->getName(),
+            'phone'     => $billingAddress->getTelephone(),
+            'email'     => $billingAddress->getEmail()
+        ];
+
+        // Add contact information
+        foreach ($fields as $type => $value) {
+            $gateway->addField($type, $value);
+        }
+
+        try {
+            // Create payrexx instance
+            $payrexx = $this->getPayrexxInstance();
+            // Create payrexx gateway
+            return $payrexx->create($gateway);
+        } catch (\Payrexx\PayrexxException $e) {
+            return;
         }
     }
 
     /**
-     * Get payrexx payment page url
+     * Get language based URL.
+     *
+     * @param  string $url payment link
+     * @return string
      */
-    public function getPaymentUrl()
+    public function resolvePayrexxUrl($url)
     {
-        // TO DO: Create Payment Gateway
-        return "http://www.payrexx.com";
+        $resolver = $this->_objectManager->get(
+            '\Magento\Framework\Locale\ResolverInterface'
+        );
+
+        $locale = $resolver->getLocale();
+        if (empty($locale)) {
+            $locale = $resolver->getDefaultLocale();
+        }
+        $lang = strstr($resolver->getLocale(), '_', true);
+
+        // Add current/default language into URL
+        return preg_replace(
+            '/^(https:\/\/[a-z0-9.]+)\/(.*)$/',
+            '$1/'. $lang .'/$2',
+            $url
+        );
+    }
+
+    /**
+     * Set payment related information as additional info
+     *
+     * @param \Magento\Payment\Model\Info       $payment  Payment related info
+     * @param \Payrexx\Models\Response\Gateway  $response Gateway response
+     */
+    public function setPaymentAdditionalInfo($payment, $response)
+    {
+        // Generate security hash based on hash alogorithm.
+        $hash = hash_hmac(
+            'sha1',
+            $response->getHash(),
+            $this->getPayrexxConfig()['api_secret'],
+            false
+        );
+
+        $payment->setAdditionalInformation(
+            static::PAYMENT_GATEWAY_ID,
+            $response->getId()
+        );
+
+        $payment->setAdditionalInformation(
+            static::PAYMENT_SECURITY_HASH,
+            $hash
+        );
+        $payment->save();
     }
 }
