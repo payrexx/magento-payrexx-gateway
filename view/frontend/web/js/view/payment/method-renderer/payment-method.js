35e348a25d2783346006265b9caae4cfc96cdab5
vinoth_ss4u <vinoth@softsolutions4u.info>
2018-04-16 15:54:14 +0530

    Changed code alignment.

--- view/frontend/web/js/view/payment/method-renderer/payment-method.js
+++ view/frontend/web/js/view/payment/method-renderer/payment-method.js
@@ -9,84 +9,86 @@
 
 /*browser:true*/
 /*global define*/
-define([
-    'jquery',
-    'ko',
-    'Magento_Checkout/js/view/payment/default',
-    'Magento_Checkout/js/action/place-order',
-    'Magento_Checkout/js/model/payment/additional-validators',
-    'mage/url'
-],
-function (
-    $,
-    ko,
-    Component,
-    placeOrderAction,
-    additionalValidators,
-    url
-) {
-    'use strict';
+define(
+    [
+        'jquery',
+        'ko',
+        'Magento_Checkout/js/view/payment/default',
+        'Magento_Checkout/js/action/place-order',
+        'Magento_Checkout/js/model/payment/additional-validators',
+        'mage/url'
+    ],
+    function (
+        $,
+        ko,
+        Component,
+        placeOrderAction,
+        additionalValidators,
+        url
+    ) {
+        'use strict';
 
-    return Component.extend({
-        defaults: {
-            template: 'Payrexx_PaymentGateway/payment/payment-method'
-        },
+        return Component.extend({
+            defaults: {
+                template: 'Payrexx_PaymentGateway/payment/payment-method'
+            },
 
-        /**
-         * Get payment method data
-         *
-         * @returns {object}
-         */
-        getData: function() {
-            return {
-                'method': this.item.method,
-                'additional_data': {}
-            };
-        },
+            /**
+             * Get payment method data
+             *
+             * @returns {object}
+             */
+            getData: function () {
+                return {
+                    'method': this.item.method,
+                    'additional_data': {}
+                };
+            },
 
-        /**
-         * Place order
-         *
-         * @param {object} data  jQuery Ui class
-         * @param {object} event jQuery event
-         * @returns {Boolean}
-         */
-        placeOrder: function (data, event) {
-            var self = this;
+            /**
+             * Place order
+             *
+             * @param {object} data  jQuery Ui class
+             * @param {object} event jQuery event
+             * @returns {Boolean}
+             */
+            placeOrder: function (data, event) {
+                var self = this;
 
-            if (event) {
-                event.preventDefault();
-            }
+                if (event) {
+                    event.preventDefault();
+                }
 
-            if (this.validate() && additionalValidators.validate()) {
-                this.isPlaceOrderActionAllowed(false);
+                if (this.validate() && additionalValidators.validate()) {
+                    this.isPlaceOrderActionAllowed(false);
 
-                $.when(
+                    $.when(
                         placeOrderAction(this.getData(), this.messageContainer)
                     )
-                    .fail(
-                        function () {
-                            self.isPlaceOrderActionAllowed(true);
-                        }
-                    ).done(
-                        function () {
-                            self.afterPlaceOrder();
-                        }
-                    );
+                        .fail(
+                            function () {
+                                self.isPlaceOrderActionAllowed(true);
+                            }
+                        ).done(
+                            function () {
+                                self.afterPlaceOrder();
+                            }
+                        );
 
-                return true;
+                    return true;
+                }
+                return false;
+            },
+
+            /**
+             * After place order callback
+             */
+            afterPlaceOrder: function () {
+                // Redirect into controller
+                $.mage.redirect(
+                    url.build('payrexx/payment/redirect')
+                );
             }
-            return false;
-        },
-
-        /**
-         * After place order callback
-         */
-        afterPlaceOrder: function () {
-            // Redirect into controller
-            $.mage.redirect(
-                url.build('payrexx/payment/redirect')
-            );
-        }
-    });
-});
+        });
+    }
+);
