8c3dde70d328f07758f56d912aa35eb0e27cf804
vinothss4u <vinoth@softsolutions4u.info>
2022-04-01 18:37:49 +0530

    implemented payrexx payment methods

---
+++ Model/PaymentMethodElv.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Payrexx Payment Gateway
+ *
+ * Copyright © 2022 PAYREXX AG (https://www.payrexx.com)
+ * See LICENSE.txt for license details.
+ *
+ * @copyright   2022 PAYREXX AG
+ * @author      Payrexx <support@payrexx.com>
+ * @package     magento2
+ * @subpackage  payrexx_payment_gateway
+ * @version     1.0.0
+ */
+namespace Payrexx\PaymentGateway\Model;
+
+/**
+ * PaymentMethod model for Payrexx
+ *
+ * @method \Magento\Quote\Api\Data\PaymentMethodExtensionInterface getExtensionAttributes()
+ *
+ * @api
+ * @since 100.0.2
+ */
+class PaymentMethodElv extends \Magento\Payment\Model\Method\AbstractMethod
+{
+    /**
+     * @var string
+     */
+    const PAYMENT_METHOD_PAYREXX_CODE = 'payrexx_payment_elv';
+
+    /**
+     * Payment method code
+     *
+     * @var string
+     */
+    protected $_code = self::PAYMENT_METHOD_PAYREXX_CODE;
+
+    /**
+     * Run the payment initialize while order place
+     *
+     * @var bool
+     */
+    protected $_isInitializeNeeded = true;
+
+    /**
+     * @inheritdoc
+     */
+    public function initialize($paymentAction, $stateObject)
+    {
+        if ($paymentAction === static::ACTION_AUTHORIZE) {
+            $stateObject->setState(
+                \Magento\Sales\Model\Order::STATE_PENDING_PAYMENT
+            );
+            $stateObject->setStatus(
+                \Magento\Sales\Model\Order::STATE_PENDING_PAYMENT
+            );
+            $stateObject->setIsNotified(false);
+        }
+    }
+}
--- view/frontend/web/js/view/payment/payrexx-payment.js
+++ view/frontend/web/js/view/payment/payrexx-payment.js
@@ -16,29 +16,46 @@ define([
     'uiComponent',
     'Magento_Checkout/js/model/payment/renderer-list'
 ], function (Component, rendererList) {
+    var componentJs = 'Payrexx_PaymentGateway/js/view/payment/method-renderer/payment-method';
     'use strict';
 
     rendererList.push(
-        {
-            type: 'payrexx_payment',
-            component: 'Payrexx_PaymentGateway/js/view/payment/method-renderer/payment-method'
-        },
-        {
-            type: 'payrexx_payment_mastercard',
-            component: 'Payrexx_PaymentGateway/js/view/payment/method-renderer/payment-method'
-        },
-        {
-            type: 'payrexx_payment_visa',
-            component: 'Payrexx_PaymentGateway/js/view/payment/method-renderer/payment-method'
-        },
-        {
-            type: 'payrexx_payment_apple_pay',
-            component: 'Payrexx_PaymentGateway/js/view/payment/method-renderer/payment-method'
-        },
-        {
-            type: 'payrexx_payment_maestro',
-            component: 'Payrexx_PaymentGateway/js/view/payment/method-renderer/payment-method'
-        },
+        {type: 'payrexx_payment', component: componentJs},
+        {type: 'payrexx_payment_masterpass', component: componentJs},
+        {type: 'payrexx_payment_mastercard', component: componentJs},
+        {type: 'payrexx_payment_visa', component: componentJs},
+        {type: 'payrexx_payment_apple_pay', component: componentJs},
+        {type: 'payrexx_payment_maestro', component: componentJs},
+        {type: 'payrexx_payment_jcb', component: componentJs},
+        {type: 'payrexx_payment_american_express', component: componentJs},
+        {type: 'payrexx_payment_wirpay', component: componentJs},
+        {type: 'payrexx_payment_paypal', component: componentJs},
+        {type: 'payrexx_payment_bitcoin', component: componentJs},
+        {type: 'payrexx_payment_sofortueberweisung', component: componentJs},
+        {type: 'payrexx_payment_billpay', component: componentJs},
+        {type: 'payrexx_payment_bonuscard', component: componentJs},
+        {type: 'payrexx_payment_cashu', component: componentJs},
+        {type: 'payrexx_payment_cb', component: componentJs},
+        {type: 'payrexx_payment_diners_club', component: componentJs},
+        {type: 'payrexx_payment_direct_debit', component: componentJs},
+        {type: 'payrexx_payment_discover', component: componentJs},
+        {type: 'payrexx_payment_elv', component: componentJs},
+        {type: 'payrexx_payment_ideal', component: componentJs},
+        {type: 'payrexx_payment_invoice', component: componentJs},
+        {type: 'payrexx_payment_myone', component: componentJs},
+        {type: 'payrexx_payment_paysafecard', component: componentJs},
+        {type: 'payrexx_payment_postfinance_card', component: componentJs},
+        {type: 'payrexx_payment_postfinance_efinance', component: componentJs},
+        {type: 'payrexx_payment_swissbilling', component: componentJs},
+        {type: 'payrexx_payment_twint', component: componentJs},
+        {type: 'payrexx_payment_barzahlen', component: componentJs},
+        {type: 'payrexx_payment_bancontact', component: componentJs},
+        {type: 'payrexx_payment_giropay', component: componentJs},
+        {type: 'payrexx_payment_eps', component: componentJs},
+        {type: 'payrexx_payment_google_pay', component: componentJs},
+        {type: 'payrexx_payment_klarna_paynow', component: componentJs},
+        {type: 'payrexx_payment_klarna_paylater', component: componentJs},
+        {type: 'payrexx_payment_oney', component: componentJs},
     );
 
     /** Add view logic here if needed */
