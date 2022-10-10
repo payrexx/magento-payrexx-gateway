fb8f10bdd0a34691486e048dcde0a55fb3bdfa47
noahming <nm99@gmx.ch>
2022-10-10 15:15:07 +0200

    New version 1.3.0

--- Controller/Payment/Webhook.php
+++ Controller/Payment/Webhook.php
@@ -9,7 +9,6 @@
  * @author      Payrexx <support@payrexx.com>
  * @package     magento2
  * @subpackage  payrexx_payment_gateway
- * @version     1.0.1
  */
 namespace Payrexx\PaymentGateway\Controller\Payment;
 
