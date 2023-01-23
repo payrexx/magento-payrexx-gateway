2ed4793166780947018548cb4869f09f6d85ea01
vekkon <nm99@gmx.ch>
2023-01-23 13:43:23 +0100

    Added Go2Flow to platform selection

--- Model/Config/Source/Platform.php
+++ Model/Config/Source/Platform.php
@@ -32,6 +32,7 @@ class Platform implements \Magento\Framework\Data\OptionSourceInterface
             'shop-and-pay.com' => __('Shop and Pay'),
             'ideal-pay.ch' => __('Ideal Pay'),
             'payzzter.com' => __('Payzzter'),
+            'go2flow.finance' => __('Go2Flow'),
         ];
     }
 }
