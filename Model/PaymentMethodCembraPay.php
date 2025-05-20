0f919a8656bd16d53105859f0ccf17726e545ba4
vinoth <vinoth@payrexx.com>
2025-05-20 17:29:03 +0530

    task/PP-15262: added cembrapay payment method

---
+++ Model/PaymentMethodCembraPay.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Payrexx Payment Gateway Module
+ *
+ * @category  Payrexx
+ * @package   Payrexx_PaymentGateway
+ * @author    Support <support@payrexx.com>
+ * @copyright PAYREXX AG
+ */
+
+namespace Payrexx\PaymentGateway\Model;
+
+class PaymentMethodCembraPay extends PayrexxBase
+{
+    /**
+     * Payment method code
+     *
+     * @var string
+     */
+    protected $_code = 'payrexx_payment_cembrapay';
+}
